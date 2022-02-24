13a7bb78dcd0419770fc380af9725cf0367412b7
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-24 10:30:33 -0300

    Add project files.

---
+++ Tipos_Triangulos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tipos_Triangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+
+            Console.WriteLine("Digite o valor para A" );
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor para B");
+            B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor para C");
+            C = Convert.ToInt32(Console.ReadLine());
+
+            if (A == B && B == C)
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO");
+                Console.ReadLine();
+            }
+            else
+            {
+                if(A == B || A == C || B == C) 
+                {
+                    Console.WriteLine("TRIANGULO ISOSCELES");
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    Console.WriteLine("TRIANGULO ESCALENO");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
